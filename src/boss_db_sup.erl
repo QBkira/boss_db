06ce17999cd68a66099da0360aa06d6cadb285ea
qbkira <qbuouo@gmail.com>
2016-05-26 13:15:33 +0800

    support process config

--- src/boss_db_sup.erl
+++ src/boss_db_sup.erl
@@ -14,8 +14,12 @@ start_link(StartArgs) ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, StartArgs).
 
 init(StartArgs) ->
+    DBConnumber    = proplists:get_value(db_con_num, StartArgs, 10),
+    DBMaxConnumber = proplists:get_value(db_max_con_num, StartArgs, 20),
+
+    lager:info("Start Database sup con :~p. max_con: ~p", [DBConnumber, DBMaxConnumber]),
     Args = [{name, {local, boss_db_pool}},
         {worker_module, boss_db_controller},
-        {size, 50}, {max_overflow, 100}|StartArgs],
+        {size, DBConnumber}, {max_overflow, DBMaxConnumber}|StartArgs],
     PoolSpec = {db_controller, {poolboy, start_link, [Args]}, permanent, 2000, worker, [poolboy]},
     {ok, {{one_for_one, 10, 10}, [PoolSpec]}}.
